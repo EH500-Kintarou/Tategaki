2c7f8e5d6899849737bac762b33cd9dda9ea8838
EH500_Kintarou <EH500_Kintarou@example.com>
2024-04-02 23:02:25 +0900

    TategakiTextをFrameworkElement直接継承とした。 グリフインデックスのキャッシュ機構を書き直した（主にCultureInfoに依存しないようにした）

---
+++ Tategaki/Logic/FontUriTable.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using WaterTrans.TypeLoader;
+
+namespace Tategaki.Logic
+{
+	/// <summary>
+	/// フォント名をUriに変換するテーブル
+	/// </summary>
+	internal class FontUriTable
+	{
+		static readonly SortedDictionary<string, Uri> _AllVerticalFonts;
+		static readonly SortedDictionary<string, Uri> _AllAdvancedVerticalFonts;
+		static readonly Dictionary<CultureInfo, IDictionary<string, Uri>> _CultureVerticalFonts;
+		static readonly Dictionary<CultureInfo, IDictionary<string, Uri>> _CultureAdvancedVerticalFonts;
+
+		/// <summary>
+		/// 縦書きフォントを含むすべてのカルチャーのフォント名をUriに変換するテーブル
+		/// </summary>
+		internal static IDictionary<string, Uri> AllVerticalFonts => _AllVerticalFonts;
+
+		/// <summary>
+		/// Advanced縦書きフォントを含むすべてのカルチャーのフォント名をUriに変換するテーブル
+		/// </summary>
+		internal static IDictionary<string, Uri> AllAdvancedVerticalFonts => _AllAdvancedVerticalFonts;
+
+		/// <summary>
+		/// 特定のカルチャーの縦書きフォント名-Uriのテーブルを取得するテーブル
+		/// </summary>
+		internal static IDictionary<CultureInfo, IDictionary<string, Uri>> CultureVerticalFonts => _CultureVerticalFonts;
+
+		/// <summary>
+		/// 特定のカルチャーのAdvanced縦書きフォント名-Uriのテーブルを取得するテーブル
+		/// </summary>
+		internal static IDictionary<CultureInfo, IDictionary<string, Uri>> CultureAdvancedVerticalFonts => _CultureAdvancedVerticalFonts;
+
+		static FontUriTable()
+		{
+			_AllVerticalFonts = new();
+			_AllAdvancedVerticalFonts = new();
+			_CultureVerticalFonts = new();
+			_CultureAdvancedVerticalFonts = new();
+
+			string fontDir = Environment.GetFolderPath(Environment.SpecialFolder.Fonts);
+			var uris = Directory.GetFiles(fontDir, "*.ttf").Concat(Directory.GetFiles(fontDir, "*.otf")).Select(p => new Uri(p))
+				.Concat(Directory.GetFiles(fontDir, "*.ttc").SelectMany(p => {
+					using(var fs = new FileStream(p, FileMode.Open, FileAccess.Read)) {
+						return Enumerable.Range(0, TypefaceInfo.GetCollectionCount(fs)).Select(i => new UriBuilder("file", "", -1, p, "#" + i).Uri);
+					}
+				})
+			);
+
+			foreach(Uri uri in uris) {
+				try {
+					var gtf = new GlyphTypeface(uri);
+
+					foreach(var (culture, name) in gtf.FamilyNames) {
+						if(culture == null || name == null) continue;
+
+						int num = uri.Fragment == "" ? 0 : int.Parse(uri.Fragment.Replace("#", ""));
+						var info = new TypefaceInfo(gtf.GetFontStream(), num);
+
+						if(info.GetVerticalGlyphConverter().Count != 0) {
+							_AllVerticalFonts[name] = uri;
+
+							IDictionary<string, Uri> culturedic;
+
+							if(_CultureVerticalFonts.ContainsKey(culture))
+								culturedic = _CultureVerticalFonts[culture];
+							else {
+								culturedic = new Dictionary<string, Uri>();
+								_CultureVerticalFonts[culture] = culturedic;
+							}
+							culturedic[name] = uri;
+						}
+
+						if(info.GetAdvancedVerticalGlyphConverter().Count != 0) {
+							_AllAdvancedVerticalFonts[name] = uri;
+
+							IDictionary<string, Uri> culturedic;
+
+							if(_CultureAdvancedVerticalFonts.ContainsKey(culture))
+								culturedic = _CultureAdvancedVerticalFonts[culture];
+							else {
+								culturedic = new Dictionary<string, Uri>();
+								_CultureAdvancedVerticalFonts[culture] = culturedic;
+							}
+							culturedic[name] = uri;
+						}
+					}
+				}
+				catch(NullReferenceException) { }
+			}
+		}
+
+		/// <summary>
+		/// フォント名からUriを取得するメソッド
+		/// 存在しない場合は適当なフォントにフォールバックする。
+		/// </summary>
+		/// <param name="name">フォント名</param>
+		/// <param name="advanced">Advancedフォントかどうか</param>
+		/// <returns></returns>
+		internal static Uri FromName(string name, bool advanced = false)
+		{
+			var dic = advanced ? _AllAdvancedVerticalFonts : _AllVerticalFonts;
+
+			if(dic.TryGetValue(name, out Uri? uri))
+				return uri;
+			else {
+				var include = dic.Keys.Where(p => p.Contains(name) || name.Contains(p)).FirstOrDefault();
+				if(include != null)
+					return dic[include];
+				else
+					return dic.First().Value;
+			}
+		}
+	}
+}
--- Tategaki/TategakiText.cs
+++ Tategaki/TategakiText.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,21 +11,29 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Tategaki.Logic;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Tategaki
 {
-	public class TategakiText : Control
+	public class TategakiText : FrameworkElement
 	{
-		Glyphs? glyph;
+		readonly VisualCollection children;
+		DrawingVisual? drawingText = null;
+		DrawingVisual? drawingBackground = null;
+		Size textSize;
+		Size availableSize;
+		GlyphRun? lastGlyph = null;
+		Rect? lastBgRect = null;
 
-		static TategakiText()
+		public TategakiText()
 		{
-			DefaultStyleKeyProperty.OverrideMetadata(typeof(TategakiText), new FrameworkPropertyMetadata(typeof(TategakiText)));
+			children = new VisualCollection(this);
 		}
 
 		#region Properties
@@ -32,8 +43,14 @@ namespace Tategaki
 		/// </summary>
 		public static string[] AvailableFonts
 		{
-			get { return Util.AvailableFonts; }
+			get
+			{
+				if(_AvailableFonts == null)
+					_AvailableFonts = FontUriTable.CultureVerticalFonts[CultureInfo.CurrentUICulture].Select(p => p.Key).ToArray();
+				return _AvailableFonts;
+			}
 		}
+		static string[]? _AvailableFonts = null;
 
 		/// <summary>
 		/// 表示テキスト
@@ -46,9 +63,49 @@ namespace Tategaki
 		public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
 			nameof(Text), typeof(string), typeof(TategakiText), new PropertyMetadata((d, e) => {
 				TategakiText me = (TategakiText)d;
-				me.RedrawText();
+				me.Redraw(true);
 			})
 		);
+
+		/// <summary>
+		/// フォントファミリー
+		/// </summary>
+		public FontFamily FontFamily
+		{
+			get { return (FontFamily)GetValue(FontFamilyProperty); }
+			set { SetValue(FontFamilyProperty, value); }
+		}
+		public static readonly DependencyProperty FontFamilyProperty = TextElement.FontFamilyProperty.AddOwner(typeof(TategakiText));
+
+		/// <summary>
+		/// フォントサイズ
+		/// </summary>
+		public double FontSize
+		{
+			get { return (double)GetValue(FontSizeProperty); }
+			set { SetValue(FontSizeProperty, value); }
+		}
+		public static readonly DependencyProperty FontSizeProperty = TextElement.FontSizeProperty.AddOwner(typeof(TategakiText));
+
+		/// <summary>
+		/// フォントの太さ
+		/// </summary>
+		public FontWeight FontWeight
+		{
+			get { return (FontWeight)GetValue(FontWeightProperty); }
+			set { SetValue(FontWeightProperty, value); }
+		}
+		public static readonly DependencyProperty FontWeightProperty = TextElement.FontWeightProperty.AddOwner(typeof(TategakiText));
+
+		/// <summary>
+		/// フォントスタイル
+		/// </summary>
+		public FontStyle FontStyle
+		{
+			get { return (FontStyle)GetValue(FontStyleProperty); }
+			set { SetValue(FontStyleProperty, value); }
+		}
+		public static readonly DependencyProperty FontStyleProperty = TextElement.FontStyleProperty.AddOwner(typeof(TategakiText));
 
 		/// <summary>
 		/// 文字間隔
@@ -61,88 +118,187 @@ namespace Tategaki
 		public static readonly DependencyProperty SpacingProperty = DependencyProperty.Register(
 			nameof(Spacing), typeof(double), typeof(TategakiText), new PropertyMetadata((double)100, (d, e) => {
 				TategakiText me = (TategakiText)d;
-				me.RedrawText();
+				me.Redraw(true);
+			})
+		);
+
+		/// <summary>
+		/// 文字の色
+		/// </summary>
+		public Brush Foreground
+		{
+			get { return (Brush)GetValue(ForegroundProperty); }
+			set { SetValue(ForegroundProperty, value); }
+		}
+		public static readonly DependencyProperty ForegroundProperty = DependencyProperty.Register(
+			nameof(Foreground), typeof(Brush), typeof(TategakiText), new PropertyMetadata(SystemColors.ControlTextBrush, (d, e) => {
+				TategakiText me = (TategakiText)d;
+				me.Redraw(false);
+			})
+		);
+
+		/// <summary>
+		/// 背景色
+		/// </summary>
+		public Brush Background
+		{
+			get { return (Brush)GetValue(BackgroundProperty); }
+			set { SetValue(BackgroundProperty, value); }
+		}
+		public static readonly DependencyProperty BackgroundProperty = DependencyProperty.Register(
+			nameof(Background), typeof(Brush), typeof(TategakiText), new PropertyMetadata(Brushes.Transparent, (d, e) => {
+				TategakiText me = (TategakiText)d;
+				me.Redraw(false);
 			})
 		);
 
 		#endregion
 
-		#region Fonts
-
-		#endregion
-
 		#region Overrides
-
-		public override void OnApplyTemplate()
-		{
-			base.OnApplyTemplate();
-
-			glyph = (Glyphs)this.Template.FindName("PART_Glyphs", this);
-
-			RedrawText();
-		}
 
 		protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
 		{
 			switch(e.Property.Name) {
-				case nameof(this.FontFamily):
-					RedrawText();
+				case nameof(HorizontalAlignment):
+				case nameof(VerticalAlignment):
+					RedrawBackground();
+					InvalidateMeasure();
 					break;
-				case nameof(this.FontWeight):
-				case nameof(this.FontStyle):
-					SetStyleSimulations();
-					break;
-				case nameof(this.Visibility):
-					ChangeVisibility();
+				case nameof(FontFamily):
+				case nameof(FontSize):
+				case nameof(FontWeight):
+				case nameof(FontStyle):
+					Redraw(true);
 					break;
 			}
 			base.OnPropertyChanged(e);
+		}
+
+		protected override Size MeasureOverride(Size availableSize)
+		{
+			this.availableSize = availableSize;
+
+			if(!lastBgRect.HasValue || lastBgRect.Value.Width != availableSize.Width || lastBgRect.Value.Height != availableSize.Height) {
+				RedrawBackground();
+				UpdateLayout();
+			}
+
+			if(lastBgRect.HasValue)
+				return new Size(lastBgRect.Value.Width, lastBgRect.Value.Height);
+			else
+				return Size.Empty;	// まあまずはここには来ないので適当に
+		}
+
+		protected override int VisualChildrenCount => children.Count;
+
+		protected override Visual GetVisualChild(int index)
+		{
+			if(index < 0 || index >= children.Count)
+				throw new ArgumentOutOfRangeException(nameof(index), $"{nameof(index)} is out of range");
+
+			return children[index];
 		}
 
 		#endregion
 
 		#region Methods
 
-		void ChangeVisibility()
+		void Redraw(bool glyphChanged)
 		{
-			if(glyph != null) {
-				if(string.IsNullOrEmpty(Text))
-					glyph.Visibility = Visibility.Collapsed;
-				else
-					glyph.Visibility = this.Visibility;
-			}
+			RedrawText(glyphChanged);
+			RedrawBackground();
+
+			UpdateLayout();
+			if(glyphChanged)
+				InvalidateMeasure();			
 		}
 
-		void SetStyleSimulations()
+		void RedrawBackground()
 		{
-			if(glyph != null) {
-				glyph.StyleSimulations =
-					((FontWeight != FontWeights.Normal) ? StyleSimulations.BoldSimulation : StyleSimulations.None) |
-					((FontStyle != FontStyles.Normal) ? StyleSimulations.ItalicSimulation : StyleSimulations.None);
+			if(drawingBackground != null) {
+				children.Remove(drawingBackground);
+				drawingBackground = null;
 			}
+
+			var drawing = new DrawingVisual();
+			DrawingContext context = drawing.RenderOpen();
+
+			var width = HorizontalAlignment switch {
+				HorizontalAlignment.Left or HorizontalAlignment.Center or HorizontalAlignment.Right
+					=> textSize.Width,
+				HorizontalAlignment.Stretch or _
+					=> double.IsPositiveInfinity(availableSize.Width) ? textSize.Width : availableSize.Width,
+			};
+
+			var height = VerticalAlignment switch {
+				VerticalAlignment.Top or VerticalAlignment.Center or VerticalAlignment.Bottom
+					=> textSize.Height,
+				VerticalAlignment.Stretch or _
+					=> double.IsPositiveInfinity(availableSize.Height) ? textSize.Height : availableSize.Height,
+			};
+
+			var rectangle = new Rect(0, 0, width, height);
+			context.DrawRectangle(Background, null, rectangle);
+
+			context.Close();
+
+			lastBgRect = rectangle;
+
+			drawingBackground = drawing;
+			children.Insert(0, drawingBackground);
 		}
 
-		void RedrawText()
-		{
-			if(glyph != null) {
-				if(!string.IsNullOrEmpty(Text)) {
-					string[] fonts = new[] {	//フォントを優先度高い順に
-						FontFamily.Source,
-						SystemFonts.MessageFontFamily.Source,
-						"ＭＳ ゴシック",
-						"ＭＳ 明朝",
-						AvailableFonts.First(),
-					};
 
-					string fontname = fonts.Where(p => AvailableFonts.Contains(p)).First();
-					glyph.FontUri = Util.GetFontUri(fontname);
-					glyph.Indices = Util.GetIndices(Text, fontname, Spacing);
-					SetStyleSimulations();
-					glyph.UnicodeString = Text;
+		void RedrawText(bool glyphChanged)
+		{
+			if(drawingText != null) {
+				children.Remove(drawingText);
+				drawingText = null;
+			}
+
+			if(!string.IsNullOrEmpty(Text)) {
+				var drawing = new DrawingVisual();
+				DrawingContext context = drawing.RenderOpen();
+
+				if(glyphChanged) {
+					var language = XmlLanguage.GetLanguage(CultureInfo.CurrentUICulture.Name);
+					var fontname = FontFamily.Source;
+					var uri = FontUriTable.FromName(fontname);
+					var face = new GlyphTypeface(uri, ((FontWeight == FontWeights.Normal) ? StyleSimulations.None : StyleSimulations.BoldSimulation) | ((FontStyle == FontStyles.Normal) ? StyleSimulations.None : StyleSimulations.ItalicSimulation));
+					var renderingEmSize = FontSize;
+					var spacing = Spacing;
+					var origin = new Point(0, 0);
+					var advanceWidth = Enumerable.Repeat<double>(renderingEmSize, Text.Length).ToArray();
+					var glyphOffset = Enumerable.Range(0, Text.Length).Select(p => new Point((double)p * (spacing - 100) / 100 * renderingEmSize, 0)).ToArray();
+					var text = Text;
+					var glyphIndices = VerticalIndicesCache.GetCache(uri).GetIndices(Text);
+
+					// 文字列の描画サイズを確認する
+					var glyphrun = new GlyphRun(face, 0, true, renderingEmSize, 1, glyphIndices, origin, advanceWidth, glyphOffset, text.ToArray(), fontname, null, null, language);
+					var glyphbox = glyphrun.ComputeAlignmentBox();
+
+					// 文字の高さ分右に動かして、かつ左上を原点とする
+					origin.Offset(glyphbox.Height, -glyphbox.Y);
+					glyphrun = new GlyphRun(face, 0, true, renderingEmSize, 1, glyphIndices, origin, advanceWidth, glyphOffset, text.ToArray(), fontname, null, null, language);
+
+					lastGlyph = glyphrun;
+					textSize = new Size(glyphbox.Height, glyphbox.Width + text.Length * (double)(spacing - 100) / 100 * renderingEmSize);   // 回転後のサイズ
 				}
-				ChangeVisibility(); //文字列がemptyだとGlyphsが例外を吐くのでVisibilityでごまかす
+				context.DrawGlyphRun(Foreground, lastGlyph);
+				context.Close();
+
+				drawing.Clip = new RectangleGeometry(new Rect(textSize.Width, 0, textSize.Height, textSize.Width)); // 回転前の寸法でクリッピングする
+				drawing.Transform = new RotateTransform(90, textSize.Width, 0); // 起点は文字幅分右に行ったところ
+
+				children.Add(drawing);
+
+				drawingText = drawing;
+			} else {
+				lastGlyph = null;
+				textSize = new Size();
 			}
 		}
+
 		#endregion
 
 	}
