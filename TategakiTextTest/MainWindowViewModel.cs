194b29ad9e88a79301f818e28bb7f83c1b7e3dc8
EH500_Kintarou <EH500_Kintarou@example.com>
2024-04-03 18:33:18 +0900

    「基本」タブを実装

---
+++ TategakiTextTest/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace TategakiTextTest
+{
+	public class MainWindowViewModel : INotifyPropertyChanged
+	{
+		DispatcherTimer timer = new DispatcherTimer();
+
+		public MainWindowViewModel()
+		{
+			timer.Tick += Timer_Tick;
+			timer.Interval = TimeSpan.FromSeconds(1);
+			timer.Start();
+		}
+
+		private void Timer_Tick(object? sender, EventArgs e)
+		{
+			ChangingText = ChangingText switch {
+				"変化する" => "テキスト",
+				_ => "変化する",
+			};
+		}
+
+		public string? ChangingText
+		{
+			get { return _ChangingText; }
+			set
+			{
+				if(_ChangingText == value)
+					return;
+				_ChangingText = value;
+				RaisePropertyChanged();
+			}
+		}
+		private string? _ChangingText;
+
+		#region INotifyPropertyChanged
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		#endregion
+	}
+}
