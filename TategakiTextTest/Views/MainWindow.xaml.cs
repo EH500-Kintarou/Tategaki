1840feb7a11abbce298c9ecb5d5d44dfd88aab81
EH500_Kintarou <EH500_Kintarou@example.com>
2024-04-03 23:43:58 +0900

    DocumentViewerのUIテストを実装

--- TategakiTextTest/Views/MainWindow.xaml.cs
+++ TategakiTextTest/Views/MainWindow.xaml.cs
@@ -19,6 +19,17 @@ namespace TategakiTextTest.Views
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			DocCtrl docctrl = new DocCtrl();
+
+			var doc = new FixedDocument();
+			var page = new FixedPage() { Height = 1122.52, Width = 793.7 };
+			var pageContent = new PageContent();
+			page.Children.Add(docctrl);
+			pageContent.Child = page;
+			doc.Pages.Add(pageContent);
+
+			documentViewer.Document = doc;
 		}
 	}
 }
