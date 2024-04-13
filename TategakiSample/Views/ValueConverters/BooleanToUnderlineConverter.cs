2c2cba30d5744ba87a4ab309ac8165b9ad8a56fa
EH500_Kintarou <EH500_Kintarou@example.com>
2024-04-13 14:08:13 +0900

    デモにUnderlineを追加

---
+++ TategakiSample/Views/ValueConverters/BooleanToUnderlineConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Globalization;
+
+namespace TategakiSample.Views.ValueConverters
+{
+	[ValueConversion(typeof(bool), typeof(TextDecoration))]
+
+	public class BooleanToUnderlineConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if(value is bool val)
+				return val ? new TextDecorationCollection() { TextDecorations.Underline } : new TextDecorationCollection();
+			else
+				return DependencyProperty.UnsetValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if(value is TextDecorationCollection val)
+				return val.Count > 0;
+			else
+				return false;
+		}
+	}
+}
